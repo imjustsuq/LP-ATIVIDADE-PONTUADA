415b302ed3b74f1202c3bd1c73d2d68a247fecfb
tsu <safiranascimento04@gmail.com>
2026-03-10 16:47:15 -0300

    Update questao10.py

--- questao10.py
+++ questao10.py
@@ -32,22 +32,3 @@ valor_pagar = total - desconto
 
 print(f'Valor a pagar: R$ {valor_pagar:.2f}')
 
-    
-# desconto__ate_25_litros_alcool = litro_alcool * 0.02
-# desconto__acima_25_litros_alcool = litro_alcool * 0.04
-# desconto__ate_25_litros_gasolina = litro_gasolina * 0.03
-# desconto__acima_25_litros_gasolina = litro_gasolina * 0.05
-# if litros > 25 and combustivel == 'a':
-#     total = litros * litro_alcool
-#     print(total)
-# elif litros < 25 and combustivel == 'a':
-#     total = desconto__ate_25_litros_alcool
-#     print(total)
-# elif litros > 25 and combustivel == 'g':
-#     total = desconto__acima_25_litros_gasolina
-#     print(total)
-# elif litros < 25 and combustivel == 'g':
-#     total = desconto__ate_25_litros_gasolina
-#     print(total)
-    
-# valor_pagar = total - desconto
